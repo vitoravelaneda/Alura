aa2992a5b38471d12a01f54c849467598d04669b
Vitor Avelaneda <avelaneda.vitor@gmail.com>
2022-12-16 17:51:06 -0300

    include comentareis

--- curso_liguagem_c/projeto/jogo_adivinhacao.c
+++ curso_liguagem_c/projeto/jogo_adivinhacao.c
@@ -1,20 +1,23 @@
 /* AULA 2 - BEM VIDO AO JOGO DE ADIVINHAÇÂO*/
 
-#include <stdio.h>
+#include <stdio.h> // Inclui o pacote de input e output padrão
 
+// cria uma função principal
 int main (){
-    //imprime cabeçalho do jogo!
+
+    // imprimindo o cabeçalho do jogo:
     printf("*********************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação!!! *\n");
     printf("*********************************************\n");
 
-    int number_secret = 42;
-
+    int number_secret = 42; // Atribui um valor para o número secreto
+    
+    // Printa o número secreto
     /*printf ("O número %d é o secreto. Não conta pra ninguém!\n", number_secret);*/
 
-    int chute;
-
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi: %d\n", chute);
+    int chute; // Cria uma variavel (chute)
+    
+    printf("Qual é o seu chute? "); // Printa mensagem na tela
+    scanf("%d", &chute); // Atribui o valor digitado pelo usuario para veriavel chute
+    printf("Seu chute foi: %d\n", chute); // printa o valor atribuido para variavel chute
 }
