481f00ff88dfc8303b905d3827174cd971c21670
Vitor Avelaneda <avelaneda.vitor@gmail.com>
2022-12-16 03:28:57 -0300

    commit final hello world

---
+++ curso_liguagem_c/hello_world.c
@@ -0,0 +1,11 @@
+/* Código introdutório a linguagem C*/
+
+/* inclui (#include) a biblioteca stdio.h (<stdio.h>), biblioteca padrão de entrada e saída de dados em C*/
+#include <stdio.h>  
+
+/*Incluindo a Função principal (main)*/
+int main(){
+    printf("Olá, Mundo!\n"); /*(\n) faz a quebra de linha*/
+
+    return 0;
+}
